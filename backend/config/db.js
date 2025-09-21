64e063100b537e7b82bc1efab4c5781f22229280
Kumar Yadwendra <109735129+Yadwendra@users.noreply.github.com>
2025-09-21 17:41:52 +0530

    db changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,10 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+   const conn = await mongoose.connect(
+  process.env.MONGO_URI || "mongodb://localhost:27017/user_management"
+);
+
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (err) {
     console.error(`Error: ${err.message}`);
