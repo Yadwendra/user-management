0bcfbc6ae02d9cc908dc8a68200bff311b1f6f0e
Kumar Yadwendra <109735129+Yadwendra@users.noreply.github.com>
2025-09-21 12:22:07 +0530

    production ready app

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,10 @@
 // server.js
 const express = require('express');
 const cors = require('cors');
-const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const connectDB = require('./config/db.js');
 const usersRouter = require('./routes/users');
+const path = require('path')
 
 dotenv.config();
 const app = express();
@@ -14,18 +15,20 @@ app.use(express.json());
 
 // Environment variables
 const PORT = process.env.PORT || 5000;
-const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/user_management';
+
 
 // MongoDB connection
-mongoose.connect(MONGO_URI)
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.error('MongoDB connection error:', err));
+connectDB()
 
+const _dirname = path.resolve();
 // Routes
 app.use('/api/users', usersRouter);
 
-// Simple root route
-app.get('/', (req, res) => res.send('User Management API is running'));
+
+app.use(express.static(path.join(_dirname,'/frontend/dist')))
+app.get('*',(_,res)=>{
+  res.sendFile(path.resolve(_dirname,"frontend",'dist', 'index.html'))
+})
 
 // Start server
 app.listen(PORT, () => {
